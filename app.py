94b4354b6a430516789b3f6817d11f07b3f3412e
PROFILE <121040103@upq.edu.mx>
2023-06-26 07:20:31 -0600

    Creación de la pagina para insertar datos en la tabla Medicos

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from flask_mysqldb import MySQL
 
 #inicialización del framework (app)
 app= Flask(__name__)
+#ingreso de las credenciales para el acceso a la bd
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='proyecto_medicos' 
+app.secret_key='mysecretkey'
+mysql= MySQL(app)
 
 #declaración de la ruta principal (index) http://localhost:5000
 @app.route('/')
@@ -22,6 +29,26 @@ def exploraciones():
 @app.route('/pacientes')
 def pacientes():
     return render_template('pacientes.html')
+
+@app.route('/addmedicos')
+def addmedicos():
+    return render_template('addmedicos.html')
+
+@app.route('/addmedicosguardar', methods=['POST'])
+def addmedicosguardar():
+    if request.method == 'POST':
+        # pasamos a variables el contenido de los input
+        Vnombre= request.form['txtnombre']
+        Vap= request.form['txtap']
+        Vam= request.form['txtam']
+        Vrfc=request.form['txtrfc']
+        Vcedula=request.form['txtcedula']
+        Vemail=request.form['txtemail']
+        # Conectar y ejecutar el insert
+        CS = mysql.connection.cursor() # objeto de tipo cursor
+        CS.execute('insert into medicos (nombre, ap, am, rfc, cedula, email) values (%s, %s, %s, %s, %s, %s)',(Vnombre, Vap, Vam, Vrfc, Vcedula, Vemail))
+        mysql.connection.commit()
+    return redirect(url_for('addmedicos'))
 
 #ejecución del servidor en el puerto 5000
 if __name__ == '__main__':
